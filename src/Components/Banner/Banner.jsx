59f033b7508af41c6b0afc3682d0238e12654188
Rayhan Shorker <raihanshorker56@gmail.com>
2024-01-02 21:25:55 +0600

    added about page

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,16 +1,24 @@
+import { Parallax } from "swiper/modules";
 import bannerImg from "../../assets/empty-street-modern-city.jpg"
-
+// its code for implimenting Parallax manually
+// let text = document.getElementById('text');
+// window.addEventListener('scroll',()=>{
+//     let value = window.scrollY;
+//     text.style.marginTop=value*2.5+'px';
+// })
 const Banner = ({tittle}) => {
+
     return (
         <div>
             <div>
                 <img className="md:h-[500px] w-full" src={bannerImg} alt="" />
             </div>
             <div className="relative -top-36 md:-top-64">
-                <p className="font-extrabold font-serif text-3xl text-center">{tittle}</p>
+                <p id="text" className="font-extrabold font-serif text-3xl text-center">{tittle}</p>
             </div>
         </div>
     );
+
 };
 
 export default Banner;
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,52 @@
 import Banner from "../../Components/Banner/Banner";
-
+import Story from "../../../public/Story/Our Story.pdf";
+import img1 from "../../assets/vertical-low-angle-shot-high-rise-skyscrapers-glass-facade-frankfurt-germany.jpg";
+import img2 from "../../assets/beautiful-city.jpg";
 
 const About = () => {
-    return (
+  return (
+    <div >
+      <Banner tittle={"About Us"}></Banner>
+      <p className="text-center font-bold text-3xl mt-10 mb-10">Our Story</p>
+      <div className="md:flex md:justify-around justify-center px-10  gap-20 md:px-20">
         <div>
-          <Banner tittle={"About"} ></Banner>
-          <h1>About page </h1>  
+          <img className=" md:h-[900px] md:w-[2100px]" src={img1} alt="" />
         </div>
-    );
+        <div className="py-10">
+        
+          <p className="font-thin text-xl">
+            New district in Old Tbilisi is one more large project of Alpha
+            Properties. Garden Hills unites 30 000 sq.m. area territory in
+            historical area of Tbilisi, particularly, in Chugureti district.<br/> <br/>
+            With Garden Hills project, Alpha Property offers to users
+            environment adapted to features of modern district, which brings
+            together all those spaces which a resident needs today for full
+            comfort. Complex consists of 8 dwelling houses, which include 300<br/> <br/> 
+            apartments and commercial areas. First stage of Garden Hills
+            provides construction of 4 five-storey dwelling houses with 186
+            apartments in total. Most important elements of the project are
+            recreational and green spaces. Positioning of the new district will<br/> <br/> 
+            be realized on 4 season constantly green yard and recreation spaces
+            arranged on the territory. You can meet all needed infrastructure on
+            the project’s territory and what is the most important, it provides
+            comfort and meets needs of residents of all ages. Additional
+            advantage of Garden Hills is also nearness of touristic sights of
+            Tbilisi. The territory also has a kindergarten. Therefore, the
+            project is attractive as from residential, so from investment
+            standpoints.
+          </p>
+        <div className="mt-10">
+        <a href={Story} download={Story}>
+            <button className="rounded-md text-white bg-[#F85A47] md:w-48 w-36 h-16 md:h-16">
+              Download PDF
+            </button>
+          </a>
+        </div>
+        <img className="md:h-[500px] w-full mt-10" src={img2} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
